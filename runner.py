3fbca687b1cf2e3687d0c5e9e46e6112ead3331b
anuragunnikannan <anuragunnikannan7@gmail.com>
2025-03-22 17:43:50 +0530

    fixed file path

--- runner.py
+++ runner.py
@@ -3,7 +3,6 @@ import sys
 
 mode = sys.argv[0]
 language = sys.argv[1]
-user_uuid = sys.argv[2]
 
 if mode == "run":
     input_file = ""
@@ -11,13 +10,13 @@ if mode == "run":
     stderr = ""
     output = ""
 
-    with open(f"/app/{user_uuid}/input.txt", "rb") as f:
+    with open(f"/codes/input.txt", "rb") as f:
         input_file = f.read()
     
     if language == "java":
-        output = subprocess.run(["java", f"/app/{user_uuid}/Solution.java"], input=input_file, capture_output=True)
+        output = subprocess.run(["java", f"/codes/Solution.java"], input=input_file, capture_output=True)
     elif language == "python":
-        output = subprocess.run(["python3", f"/app/{user_uuid}/app.py"], input=input_file, capture_output=True)
+        output = subprocess.run(["python3", f"/codes/app.py"], input=input_file, capture_output=True)
 
     stdout = output.stdout.decode().strip()
     stderr = output.stderr.decode().strip()
